9107890580cd6da568a720f6ee744d07ed3a3387
versatile309 <versatile309@gmail.com>
2020-03-16 14:43:34 +0900

    sum integer

---
+++ app/src/test/java/com/versatile/algorithm/integer_sum/IntegerSum.kt
@@ -0,0 +1,32 @@
+package com.versatile.algorithm.integer_sum
+
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.Test
+
+fun solution(a: Int, b: Int): Long {
+
+    return when (a <= b) {
+        true -> (a..b).fold(0L, {total, next -> total + next})
+        else -> (b..a).fold(0L, {total, next -> total + next})
+    }
+}
+
+
+class SolutionTest {
+
+
+    @Test
+    fun `get sum between two integer`() {
+        assertThat(
+            solution(3, 5)
+        ).isEqualTo(12)
+
+        assertThat(
+            solution(3, 3)
+        ).isEqualTo(3)
+
+        assertThat(
+            solution(5, 3)
+        ).isEqualTo(12)
+    }
+}
